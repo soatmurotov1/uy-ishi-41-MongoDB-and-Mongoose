ac83e20ce4098ce4f687831503e6181d9902f495
soatmurotov1 <soatmurotovabrorbek23@gmail.com>
2025-10-28 16:44:16 +0500

    fix: password required validation error fixed

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import mongoose from "mongoose"; 
 import router from "./src/router/index.js";
 import { errorHandler } from "./src/middleware/errorHeander.js";
 
 dotenv.config();
 
-const app = express();
+const app = express()
 const PORT = process.env.PORT || 4000
 const mongodb = process.env.MONGO_URL
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
-app.use(errorHandler)
 
+mongoose.connect(mongodb)
+  .then(() => console.log("mongodb connected"))
+  .catch((err) => console.log("mongodb connection error:", err.message));
 
 app.use("/", router);
 
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- src/validation/customer.validation.js
+++ src/validation/customer.validation.js
@@ -1,7 +1,15 @@
-import x, { z } from "zod"
+import { z } from "zod";
 
 export const customerValidation = z.object({
-  name: z.string().nonempty({ message: "name bo'sh bulishi mumkin emas" }),
-  phone: z.string().nonempty({ message: "phone bo'sh bulishi mumkin emas" }),
-  email: z.string().email({ message: "email noto'g'ri formatda" }).optional(),
-})
+  name: z.string()
+    .min(3, { message: "name kamida 3 ta bulishi kerak" })
+    .nonempty({ message: "name bo'sh bulishi mumkin emas" }),
+
+  phone: z.string()
+    .min(7, { message: "phone kamida 7 ta bulishi kerak" })
+    .nonempty({ message: "phone bo'sh bulishi mumkin emas" }),
+  email: z.string().email({ message: "email noto'g'ri formatda" }),
+  password: z.string().min(5, { message: "password kamida 5 ta bulishi kerak" }),
+
+  role: z.enum(["admin", "manager", "staff", "customer", "user"]).optional()
+});
